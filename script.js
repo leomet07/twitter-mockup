f2fcbc67a2670409668e16e00a861469b9714fef
leomet07 <leomet07@gmail.com>
2019-04-22 20:26:37 -0400

    Very basic post

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-var maintext = document.getElementById('maintext');
+var description = document.getElementById('description');
+var title = document.getElementById('title');
 var submitbtn = document.getElementById('submit');
 
 var fireheading = document.getElementById('fireheading');
-var firebaseheadingref = firebase.database().ref().child('Heading');
+var firebaseheadingref = firebase.database().ref().child('posts');
 firebaseheadingref.on('value', function(datasnapshot) {
-	console.log(JSON.stringify(datasnapshot));
-
 	//to loop through nested
 	var entries = Object.entries(datasnapshot.val());
-	console.log(entries);
+
 	for (i = 0; i < entries.length; i++) {
 		console.log(entries[i]);
+		if (i == 0) {
+			fireheading.innerHTML = entries[i] + '<br>';
+		} else {
+			fireheading.innerHTML += entries[i] + '<br>';
+		}
 	}
-	fireheading.innerText = JSON.stringify(datasnapshot.val());
+	//fireheading.innerText = JSON.stringify(datasnapshot.val());
 });
 
 function submitclick() {
 	//adding data
-	var firebaseRef = firebase.database().ref();
-	var messagetext = maintext.value;
+	var firebaseRef = firebase.database().ref().child('posts');
+	var messagetext = description.value;
+	var titletext = title.value;
 	//for replacing a child
 	//firebaseRef.child('Text').set(messagetext);
 
 	//making a new child everytime
-	firebaseRef.push().set(messagetext);
+	firebaseRef.child(titletext).set(messagetext);
+
+	//firebaseRef.child('Text').set(messagetext);
 
 	//retreiving data
 }
